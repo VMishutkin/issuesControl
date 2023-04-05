ddabc7edf4ec41bcd9686293a3ffc4731fd78232
VMishutkin <vladmish@list.ru>
2023-04-06 00:49:23 +0700

    Initial commit

---
+++ src/main/java/tech/info/issuescontrol/IssuesControlApplication.java
@@ -0,0 +1,13 @@
+package tech.info.issuescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuesControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/info/issuescontrol/IssuesControlApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.info.issuescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
